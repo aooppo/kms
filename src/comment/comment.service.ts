54c59fd7fcc64f3dbb9c807f6065eda0df973212
TJ <i@voox.cc>
2019-07-23 21:53:49 +0800

    improve script

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -2,10 +2,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Injectable } from '@nestjs/common';
 import { CommentEntity } from './comment.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CommentService extends TypeOrmCrudService<CommentEntity> {
-    constructor(@InjectRepository(CommentEntity) repo) {
+    constructor(@InjectRepository(CommentEntity) repo: Repository<CommentEntity>) {
         super(repo);
     }
 
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { ItemEntity } from './item.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ItemService extends TypeOrmCrudService<ItemEntity> {
-    constructor(@InjectRepository(ItemEntity) repo) {
+    constructor(@InjectRepository(ItemEntity) repo: Repository<ItemEntity>) {
         super(repo);
     }
 }
